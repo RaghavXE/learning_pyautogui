9a0f438beea0217524dcc58b9c255b69447a2be1
Raghav Soni <raghav7434@gmail.com>
2025-07-20 00:26:24 +0530

    Initial commit

---
+++ pag1.py
@@ -0,0 +1,38 @@
+import os
+import time
+import pyautogui as p
+
+os.system('cls') # Clears terminal on Windows
+
+#If you move your mouse to the top-left corner of the screen (0, 0),
+# the script immediately raises an exception and stops.
+p.FAILSAFE = True
+
+print("Current mouse position:")
+print(p.position())  # Shows Point(x=?, y=?)
+
+print("Screen size:")
+print(p.size())  # Shows Size(width=?, height=?)
+
+print("Is (1292, 148) on screen?")
+print(p.onScreen(1292, 148))  # True or False
+
+# p.PAUSE = 2.5  # Pause after pyautogui actions (not print statements)
+
+# If you want a pause BEFORE checking mouse position again
+# time.sleep(2.5)
+
+print("Mouse position again after 2.5 seconds:")
+print(p.position())
+p.PAUSE = 2.5
+
+
+p.moveTo(500, 500, duration=4)  # move mouse to XY coordinates over num_second seconds
+
+
+p.moveRel(200, 200, duration=2) # move mouse relative to its current position
+# So if your mouse is at (100, 100) and you run moveRel(200, 200), it will go to:
+# New position = (100+200, 100+200) = (300, 300)
+
+
+
